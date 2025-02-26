a4e7dff10602324db917ef1c139fb143b6109f30
Robin Grindrod <robingrindrod@live.ie>
2025-02-26 14:15:03 +0000

    Add error messages to deploy script
    This should make failures easier to troubleshoot.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# Check if required environment variables are set
+if [ -z "$FTP_USERNAME" ]; then
+  echo "Error: FTP_USERNAME must be set."
+  exit 1
+fi
+
+if [ -z "$FTP_PASSWORD" ]; then
+  echo "Error: FTP_PASSWORD must be set."
+  exit 1
+fi
+
+if [ -z "$FTP_SERVER" ]; then
+  echo "Error: FTP_SERVER must be set."
+  exit 1
+fi
+
 site_dir="$(dirname "$0")/../jekyll_files/_site"
 
 lftp -c "
